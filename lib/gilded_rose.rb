ddfb0b1df45f7a62c63acb8aacc81cb3597f0416
Kat Hicks <kmhicks92@gmail.com>
2017-03-07 14:57:18 +0000

    Moved extracted methods from item to gilded_rose class in line with the spec

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,36 +8,64 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      item.update_sell_in(-1) unless item.is_sulfuras?
+      change_sell_in(item, -1) unless is_sulfuras?(item)
 
-      if !item.is_aged_brie? and !item.is_backstage_pass?
-        if !item.is_sulfuras?
-          item.update_quality(- 1)
+      if !is_aged_brie?(item) and !is_backstage_pass?(item)
+        if !is_sulfuras?(item)
+          change_quality(item, - 1)
         end
       else
-        item.update_quality(1)
-        if item.is_backstage_pass?
+        change_quality(item, 1)
+        if is_backstage_pass?(item)
           if item.sell_in < 11
-              item.update_quality(1)
+              change_quality(item, 1)
           end
           if item.sell_in < 6
-              item.update_quality(1)
+              change_quality(item, 1)
           end
         end
       end
       if item.sell_in < 0
-        if !item.is_aged_brie?
-          if !item.is_backstage_pass?
-            if !item.is_sulfuras?
-              item.update_quality(- 1)
+        if !is_aged_brie?(item)
+          if !is_backstage_pass?(item)
+            if !is_sulfuras?(item)
+              change_quality(item, - 1)
             end
           else
             item.quality = item.quality - item.quality
           end
         else
-          item.update_quality(1)
+          change_quality(item, 1)
         end
       end
     end
+  end
+
+  private
+
+  def is_aged_brie?(item)
+    item.name == "Aged Brie"
+  end
+
+  def is_sulfuras?(item)
+    item.name == "Sulfuras, Hand of Ragnaros"
+  end
+
+  def is_backstage_pass?(item)
+    item.name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def change_quality(item, value)
+    if quality_within_range?(item)
+      item.quality += value
+    end
+  end
+
+  def change_sell_in(item, value)
+    item.sell_in += value
+  end
+
+  def quality_within_range?(item)
+    item.quality > 0 && item.quality < 50
   end
 end
--- lib/item.rb
+++ lib/item.rb
@@ -11,30 +11,4 @@ class Item
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 
-  def is_aged_brie?
-    self.name == "Aged Brie"
-  end
-
-  def is_sulfuras?
-    self.name == "Sulfuras, Hand of Ragnaros"
-  end
-
-  def is_backstage_pass?
-    self.name == "Backstage passes to a TAFKAL80ETC concert"
-  end
-
-  def update_quality(value)
-    if quality_within_range?
-      self.quality += value
-    end
-  end
-
-  def update_sell_in(value)
-    self.sell_in += value
-  end
-
-  def quality_within_range?
-    self.quality > 0 && self.quality < 50
-  end
-
 end
