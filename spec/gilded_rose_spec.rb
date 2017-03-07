9ab59992558b9c43b2d3a3c48f110c116ecf32af
Kat Hicks <kmhicks92@gmail.com>
2017-03-07 14:11:49 +0000

    Added a context block for when item is 'sulfuras'

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -61,6 +61,22 @@ describe GildedRose do
 
     end
 
+    context "when item is 'sulfuras'" do
+
+      it "does not change the quality" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+        GildedRose.new(items).update_quality()
+        expect( items[0].quality ).to eq( 80 )
+      end
+
+      it "does not change the sell-in value" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+        GildedRose.new(items).update_quality()
+        expect( items[0].sell_in ).to eq( 0 )
+      end
+
+    end
+
   end
 
 end
